56f6ed8802d8c0ca2af1b21acc2e54a3305280c7
Prashant R <smplsoln@gmail.com>
2021-11-10 04:50:09 -0600

    Add exercise_7 soln

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,30 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true }
+  validates_inclusion_of :hourly_rate, :in => 40..200
+  validates_associated :store
+end
+
+class Store < ActiveRecord::Base
+  validates_length_of :name, minimum: 3
+  validates :annual_revenue, numericality: { only_integer: true }
+  validates_numericality_of :annual_revenue, greater_than_or_equal_to: 0
+  validate :must_cary_mens_or_womens_apparel
+
+  def must_cary_mens_or_womens_apparel
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, :womens_apparel, "Store must carry mens or womens apparel!")
+    end
+  end
+end
+
+errors = Store.create(name: "Surrey").errors.full_messages
+puts "Errors: #{errors.count}"
+errors.each do |err|
+  puts "Error: #{err}"
+end
